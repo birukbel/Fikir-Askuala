60047bfd16aa723b6cc12af74667f8671e73d52d
birukbel <birukbel22@gmail.com>
2023-05-29 11:26:44 +0300

    login popup

---
+++ js/db.js
@@ -0,0 +1,22 @@
+// db.js
+
+const mysql = require('mysql2');
+
+// Database configuration
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database_name'
+});
+
+// Establish the connection
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+module.exports = connection;
---
+++ js/script.js
@@ -0,0 +1,51 @@
+// Add JavaScript code to handle the click events for "Login" "Forgot Password" and "Sign Up" links
+
+document.getElementById('loginButton').addEventListener('click', function() {
+    document.getElementById('loginPopup').style.display = 'block';
+  });
+  
+  document.getElementById('loginPopup').addEventListener('click', function(e) {
+    if (e.target === this) {
+      this.style.display = 'none';
+    }
+  });
+  
+  document.getElementById('forgotPassword').addEventListener('click', function(e) {
+    e.preventDefault();
+    // Add your logic for the "Forgot Password" functionality
+    alert('Forgot Password clicked');
+  });
+  
+  document.getElementById('signUp').addEventListener('click', function(e) {
+    e.preventDefault();
+    // Add your logic for the "Sign Up" functionality
+    alert('Sign Up clicked');
+  });
+  
+
+const db = require('./db.js');
+
+// Example query in a different file
+db.query('SELECT * FROM users', (err, results) => {
+  if (err) {
+    console.error('Error executing the query:', err);
+    return;
+  }
+  console.log('Results:', results);
+});
+
+
+
+
+// database connection in the backend code
+
+const db = require('./db.js');
+
+// Example query
+db.query('SELECT * FROM users', (err, results) => {
+  if (err) {
+    console.error('Error executing the query:', err);
+    return;
+  }
+  console.log('Results:', results);
+});
